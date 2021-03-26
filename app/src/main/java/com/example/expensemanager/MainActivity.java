77c20106c57db1cb37c863800eb404093d188689
Sagar Kulkarni <sagarkulkarnioct27@gmail.com>
2021-03-26 22:51:20 +0530

    Commit

--- app/src/main/java/com/example/expensemanager/MainActivity.java
+++ app/src/main/java/com/example/expensemanager/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.expensemanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        btn.findViewById(R.id.btn1);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,screen3.class);
+                startActivity(intent);
+           }
+        });
     }
 }
